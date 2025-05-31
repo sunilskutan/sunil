303add855cae883db55bc0e1dc38a81158ee9b53
sunilskutan <sunilskutan@gmail.com>
2025-05-31 17:58:12 +0530

    Add files via upload

---
+++ fbc.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEVLQe8XTKsOyqGKBc7T8s41qbcIyxJTI",
+    authDomain: "btckmr.firebaseapp.com",
+    projectId: "btckmr",
+    storageBucket: "btckmr.firebasestorage.app",
+    messagingSenderId: "1038756102997",
+    appId: "1:1038756102997:web:e4a4924826c44bf432e5dc",
+    measurementId: "G-E73FZRNCFM"
+  };
+
+ 
